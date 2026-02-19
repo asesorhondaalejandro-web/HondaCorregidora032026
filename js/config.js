a9af289e4ea9d1aa0cd4a426a5d72e85f18e7712
asesorhondaalejandro-web <asesor.honda.alejandro@gmail.com>
2026-02-19 22:57:56 +0000

    agente

--- js/config.js
+++ js/config.js
@@ -1,12 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// js/config.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// NOTA: No importamos nada aquí. Solo exportamos el objeto de configuración.
+// Los otros archivos se encargarán de inicializar Firebase.
+
+export const firebaseConfig = {
   apiKey: "AIzaSyDpgIHTnvsmM_412DXzHCdBIrBka27nHsM",
   authDomain: "honda-crm-system-5.firebaseapp.com",
   projectId: "honda-crm-system-5",
@@ -15,7 +12,3 @@ const firebaseConfig = {
   appId: "1:757473218168:web:b8ec39aa8ce5281f99278d",
   measurementId: "G-46D81G5X5H"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
